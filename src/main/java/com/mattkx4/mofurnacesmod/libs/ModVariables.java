99711ff1ae51920c3a883cbf1195544a29daf523
Matthew Mehrtens <Mattkx9@gmail.com>
2015-04-19 20:46:42 -0500

    Added All Repository Files

---
+++ src/main/java/com/mattkx4/mofurnacesmod/libs/ModVariables.java
@@ -0,0 +1,7 @@
+package com.mattkx4.mofurnacesmod.libs;
+
+public class ModVariables {
+	public static final String MODID = "mofurnacesmod";
+	public static final String VERSION = "3.0.0-1.8";
+	public static final String NAME = "Mo' Furnaces Mod";
+}
