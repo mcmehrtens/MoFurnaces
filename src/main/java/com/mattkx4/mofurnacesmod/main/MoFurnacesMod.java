1a7f72bb111c3f745a06f0b432e0ebe00425b42c
Ken George <kvgeorge1@sbcglobal.net>
2015-04-24 15:56:48 -0500

    Added FML preInit method to main plugin file.

--- src/main/java/com/mattkx4/mofurnacesmod/main/MoFurnacesMod.java
+++ src/main/java/com/mattkx4/mofurnacesmod/main/MoFurnacesMod.java
@@ -1,14 +1,19 @@
 package com.mattkx4.mofurnacesmod.main;
 
-import com.mattkx4.mofurnacesmod.libs.ModVariables;
-
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import com.mattkx4.mofurnacesmod.libs.ModVariables;
 
 @Mod(modid = ModVariables.MODID, name = ModVariables.NAME, version = ModVariables.VERSION)
 public class MoFurnacesMod {
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
     @EventHandler
     public void init(FMLInitializationEvent event) {
     	// Log Initialization Event
