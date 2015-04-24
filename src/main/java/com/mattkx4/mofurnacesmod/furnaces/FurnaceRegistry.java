f062106bb2558062bcf3eb2a8052716d2c70b868
Ken George <kvgeorge1@sbcglobal.net>
2015-04-24 18:58:55 -0500

    Added Textures/Models for Brick Furnace and Item Registry for inventory items for Brick Furnace

--- src/main/java/com/mattkx4/mofurnacesmod/furnaces/FurnaceRegistry.java
+++ src/main/java/com/mattkx4/mofurnacesmod/furnaces/FurnaceRegistry.java
@@ -1,12 +1,18 @@
 package com.mattkx4.mofurnacesmod.furnaces;
 
-import com.mattkx4.mofurnacesmod.tileentities.TileEntityBrickFurnace;
-
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.RenderItem;
+import net.minecraft.client.resources.model.ModelBakery;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+
+import com.mattkx4.mofurnacesmod.libs.ModVariables;
+import com.mattkx4.mofurnacesmod.tileentities.TileEntityBrickFurnace;
 
 public class FurnaceRegistry {
 	// Active and Idle Furnace Variables
@@ -28,12 +34,21 @@ public class FurnaceRegistry {
 	}
 	
 	/**
+	 * Calls the item registry so the furnaces will show-up in the item/invetory lists
+	 */
+	public static void doItemRegistry() {
+		registerItems();
+	}
+	
+	/**
 	 * Initializes the furnaces and their specifications
 	 */
 	public static void initializeFurnace() {
 		// Initialize the Brick Furnace
-		GameRegistry.registerBlock((new BrickFurnace(false)).setHardness(3.5F).setStepSound(Block.soundTypePiston).setUnlocalizedName("brickfurnace_idle").setCreativeTab(CreativeTabs.tabDecorations), "brickfurnace_idle");
-        GameRegistry.registerBlock((new BrickFurnace(true)).setHardness(3.5F).setStepSound(Block.soundTypePiston).setLightLevel(0.875F).setUnlocalizedName("brickfurnace_active"), "brickfurnace_active");
+		BrickFurnaceIdle = new BrickFurnace(false).setHardness(3.5F).setStepSound(Block.soundTypePiston).setUnlocalizedName("brickfurnace_idle").setCreativeTab(CreativeTabs.tabDecorations);
+		BrickFurnaceActive = new BrickFurnace(true).setHardness(3.5F).setStepSound(Block.soundTypePiston).setLightLevel(0.875F).setUnlocalizedName("brickfurnace_active");
+		GameRegistry.registerBlock(BrickFurnaceIdle, ModVariables.MODID + ":" + "brickfurnace_idle");
+        GameRegistry.registerBlock(BrickFurnaceActive, ModVariables.MODID + ":" + "brickfurnace_active");
 	}
 	
 	/**
@@ -41,9 +56,23 @@ public class FurnaceRegistry {
 	 */
 	public static void registerFurnace2() {
 		// Initialize the Crafting Recipe and Tile Entity Registry for the Brick Furnace
-		GameRegistry.registerTileEntity(TileEntityBrickFurnace.class, "brickfurnace");
+		GameRegistry.registerTileEntity(TileEntityBrickFurnace.class, ModVariables.MODID + ":" + "brickfurnace");
 		GameRegistry.addRecipe(new ItemStack(BrickFurnaceIdle), new Object[] {
 			"XXX", "X X", "XXX", 'X', Blocks.brick_block
 		});
+	}
+	
+	/*
+	 * private method to register items
+	 */
+	private static void registerItems() {
+		RenderItem renderItem = Minecraft.getMinecraft().getRenderItem();
+		
+		// Add Brick Furnaces
+		//
+		ModelBakery.addVariantName(Item.getItemFromBlock(BrickFurnaceIdle), new String[] { ModVariables.MODID + ":" + "brickfurnace_idle" });
+		renderItem.getItemModelMesher().register(Item.getItemFromBlock(BrickFurnaceIdle), 0, new ModelResourceLocation(ModVariables.MODID + ":" + "brickfurnace_idle", "inventory"));		
+		ModelBakery.addVariantName(Item.getItemFromBlock(BrickFurnaceActive), new String[] { ModVariables.MODID + ":" + "brickfurnace_active" });
+		renderItem.getItemModelMesher().register(Item.getItemFromBlock(BrickFurnaceActive), 0, new ModelResourceLocation(ModVariables.MODID + ":" + "brickfurnace_active", "inventory"));		
 	}
 }
--- src/main/java/com/mattkx4/mofurnacesmod/main/MoFurnacesMod.java
+++ src/main/java/com/mattkx4/mofurnacesmod/main/MoFurnacesMod.java
@@ -4,6 +4,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
 
 import com.mattkx4.mofurnacesmod.furnaces.FurnaceRegistry;
 import com.mattkx4.mofurnacesmod.libs.ModVariables;
@@ -20,5 +21,10 @@ public class MoFurnacesMod {
     public void init(FMLInitializationEvent event) {
     	// Calls the secondary registry methods
     	FurnaceRegistry.secondaryRegistry();
+    	
+		// register items for rendering
+		if (event.getSide() == Side.CLIENT) {
+			FurnaceRegistry.doItemRegistry();
+		}
     }
 }
